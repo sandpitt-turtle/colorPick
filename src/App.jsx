199f2475394df1128b1cf0e334014bba50262b57
sandpitt-turtle <wottkowicz@gmail.com>
2025-02-01 09:51:40 -0500

    Completed code with extra features

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,66 @@
-// Write your Color component here
-const Color = (props) => {
-  return <div className={props.color}></div>;
+import { useState } from 'react';
+import "./index.css"; 
+
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  return (
+    <div
+      className={`${selectedColor === color ? "selected" : ""}`}
+      style={{
+        width: "8em",
+        height: "8em",
+        margin: "5px",
+        cursor: "pointer",
+        display: "inline-block",
+        borderRadius: "50%",
+        marginTop: "2em",
+        background: color, 
+      }}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
 };
+
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
+
+const colorSetId= "color-sets";
+  const colorSet = [
+    ["red", "blue"],
+    ["orange", "purple"],
+    ["pink", "maroon"],
+    ["black", "teal"],
+    ["cyan", "magenta"],
+    ["lime", "navy"],
+    ["gold", "brown"],
+    ["violet", "coral"],
+    ["silver", "forest green"],
+    ["turquoise", "purple"],
+    ["skyblue", "darkred"],  
+    ["salmon", "indigo"],    
+    ["lavender", "chocolate"],  
+    ["seagreen", "crimson"], 
+    ["orchid", "sienna"],    
+    ["darkcyan", "goldenrod"] 
+  ];
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">
-      <Color color = "pink"/>
-      <Color color = "gVGradient"/>
-      <Color color = "green"/>
-    
-</div>
+
+      {colorSet.map(([left, right], index) => {
+        const gradient = `linear-gradient(to right, ${left}, ${right})`;
+        return (
+          <div key={index} id="colors-list">
+            <Color color={left} setSelectedColor={setSelectedColor} selectedColor={selectedColor} />
+            <Color color={gradient} setSelectedColor={setSelectedColor} selectedColor={selectedColor} />
+            <Color color={right} setSelectedColor={setSelectedColor} selectedColor={selectedColor} />
+          </div>
+        );
+      })}
     </div>
   );
 };
